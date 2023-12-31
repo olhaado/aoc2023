55dc83db3d1aad0d169073862b4f447f08972cb4
olhaado <118556396+olhaado@users.noreply.github.com>
2023-12-30 23:12:25 -0500

    day3 incorrect output

--- day03/aocday3.py
+++ day03/aocday3.py
@@ -1,5 +1,5 @@
 #----------------------AoC Day 3----------------------
-import numpy as np
+import re
 
 with open("test.txt") as file:
     data = file.read().split('\n')#split data at new lines and create list
@@ -12,34 +12,41 @@ with open("test.txt") as file:
             i = i.replace(j, k)
         new_data.append(i) #replaced all symbols with 'x' from replace_dict
 
-#print(new_data)
+adjacent_numbers = []
 
-'''for each line in the list:
-for each item in the line:
-if item in the line is a number:
-and if item in the line +/- 1 is an alpha:
-then append number to number_list'''
+for i in range(len(new_data)):
+    for j in range(len(new_data[i])):
+        if new_data[i][j] == 'x':
+            #left of x
+            if j - 1 >= 0 and new_data[i][j - 1].isdigit():
+                adjacent_numbers.append(int(new_data[i][j - 1]))
 
-data_length = len(new_data) #length of list
-number_list = []
-index_counter = 0 #counter for each line
+            #right of x
+            if j + 1 < len(new_data[i]) and new_data[i][j + 1].isdigit():
+                adjacent_numbers.append(int(new_data[i][j + 1]))
 
-#while i < data_length
+            #above x
+            if i - 1 >= 0 and new_data[i - 1][j].isdigit():
+                adjacent_numbers.append(int(new_data[i - 1][j]))
 
-for i in new_data:
-    char_counter = 0 #counter for each character in each line
-    index_counter = 0 #counter for index of line
-    for j in i:
-        if j.isnumeric():
-            if char_counter + 1 < len(i) and i[char_counter + 1].isalpha(): # if char comes after number, but within line i
-                number_list.append(j)
-            elif char_counter - 1 >= 0 and i[char_counter - 1].isalpha(): # if char comes before number, but within line i
-                number_list.append(j)
-        char_counter += 1
+            #below x
+            if i + 1 < len(new_data) and new_data[i + 1][j].isdigit():
+                adjacent_numbers.append(int(new_data[i + 1][j]))
 
+            #above left x
+            if i - 1 >= 0 and j - 1 >= 0 and new_data[i - 1][j - 1].isdigit():
+                adjacent_numbers.append(int(new_data[i - 1][j - 1]))
 
-print(number_list)
+            #above right x
+            if i - 1 >= 0 and j + 1 >= 0 and new_data[i - 1][j + 1].isdigit():
+                adjacent_numbers.append(int(new_data[i - 1][j + 1]))
 
+            #below left x
+            if i + 1 >= 0 and j - 1 >= 0 and new_data[i + 1][j-1].isdigit():
+                adjacent_numbers.append(int(new_data[i + 1][j-1]))
 
+            #below right x
+            if i + 1 >= 0 and j + 1 >= 0 and new_data[i + 1][j + 1].isdigit():
+                adjacent_numbers.append(int(new_data[i + 1][j + 1]))
 
-
+print(adjacent_numbers)
--- day03/day3-1.py
+++ day03/day3-1.py
@@ -1,26 +1,64 @@
 
-'''with open('test.txt', 'r') as file:
+with open('data3.txt', 'r') as file:
     data = file.read().splitlines()
-print(data)
+
+newdata = []
+
+#symbols to replace with x
+replace_dict= {"+" : "x", "~" : "x", "`" : "x", "!" : "x", "@" : "x",\
+                "#" : "x", "$" : "x", "%" : "x", "^" : "x", "&" : "x",\
+                "*" : "x", "/" : "x", "?" : "x", ";" : "x", ":" : "x",\
+                ">" : "x", "<" : "x", "-" : "x", "=" : "x"}
+
+#use replace_dict to make all special characters "x" in data list   
+for i in data:
+    for j, k in replace_dict.items():
+        i = i.replace(j, k)
+    newdata.append(i)
+print(newdata)
 
 #returns the previous line, current line and next line relative to current
 
 previous = ""
+partsList = []
+for num, line in enumerate(newdata):
+    #newline = line.split(".") #creates a list of non "." character
+    print(line)
 
-for num, i in enumerate(data):
-    print(f"last line: {previous}")
-    print(f"current line: {i}")
-    try:
-        print(f"next line: {data[num+1]}")
-    except:
-        print("no next line")
-    print("--------")
-    previous = i
+    delimiters = [".", "x"]
 
-print("done")
+    for delimiter in delimiters:
+        line = " ".join(line.split(delimiter))
+    newline = line.split()
 
-print()'''
+    #newline = line.replace('.', ' ').replace('x', ' ').split()
+    print(newline)
+    for numb, i in enumerate(newline):
+        if i.isdigit():
+            length = len(i)
+            indexStart = line.find(i) - 1
+            indexEnd = indexStart + length + 2
+            if indexStart < 0:
+                indexStart = 0
+            if indexEnd > len(line):
+                indexEnd = len(line) + 1
+            xinslice1 = newdata[num-1][indexStart:indexEnd].find("x")
+            xinslice2 = newdata[num][indexStart:indexEnd].find("x")
+            xinslice3 = ""
+            try:
+                xinslice3 = newdata[num+1][indexStart:indexEnd].find("x")
+            except:
+                xinslice3 = -1
+            if xinslice1 != -1 or xinslice2 != -1 or xinslice3 != -1:
+                partsList.append(int(i))
+                print(f"added part {i}")
+    print(f"-------end-line-{num+1}-------")
+    previous = newdata[num-1]
+print(partsList)
+print(sum(partsList))
 
+
+### testing individual aspects before integrating ###
 
 #separates full numbers into a list, finds their length and index in line
 '''line = ".664..598..."
@@ -29,4 +67,24 @@ for num, i in enumerate(newline):
     if i.isdigit():
         print(len(i))
         print(line.index(newline[num]))'''
+
+#finds the index of a slice of a string relative to the number
+'''line1 = "...*..*."
+line2 = "467..114"
+line3 = "...*.*.."
+
+if "114" in line2:
+    length = len("114")
+    indexStart = line2.index("114") - 1
+    indexEnd = indexStart + length + 2
+    if indexStart < 0:
+        indexStart = 0
+    if indexEnd > len(line2):
+        indexEnd = len(line2) + 1
+    xinslice1 = line1[indexStart:indexEnd].find("*")
+    xinslice2 = line2[indexStart:indexEnd].find("*")
+    xinslice3 = line3[indexStart:indexEnd].find("*")
+    if xinslice1 != -1 or xinslice2 != -1 or xinslice3 != -1:
+        print('yes')'''
+
 
