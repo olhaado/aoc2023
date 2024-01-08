bc586c4345f658fd3b225ebb80dd3a92d650cf6d
olhaado <118556396+olhaado@users.noreply.github.com>
2024-01-07 19:51:40 -0500

    removed extra lines

--- day15/day15-1.py
+++ day15/day15-1.py
@@ -1,14 +1,11 @@
 with open("data15.txt", 'r') as file:
     data = file.read().split(",")
     
-#build a dictionary to look up the number based on a given value
 asciiDict = {}
 
 for num, i in enumerate(range(32, 127)):
     j = chr(i)
     asciiDict[j] = num + 32
-print(asciiDict)
-
 
 spicyList = []
 for item in data:
@@ -17,6 +14,6 @@ for item in data:
         spicyNum += asciiDict[char]
         spicyNum = spicyNum * 17
         spicyNum = spicyNum % 256
-    #print(f"{item} = {spicyNum}")
     spicyList.append(int(spicyNum))
+    
 print(sum(spicyList))
